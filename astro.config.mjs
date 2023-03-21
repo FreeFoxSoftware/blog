7530b90154d8fdbfdce696909737b9efe90b8e83
Scott Walker <scott.david.walker@hotmail.com>
2023-03-21 19:09:10 +0000

    fix: remove old blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://blog.scottwalker.dev/",
   integrations: [
     tailwind({
       config: {
